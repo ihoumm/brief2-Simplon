e87515b620acbcffd8e39fb51018637048843e1b
ihoumm <59344957+ihoumm@users.noreply.github.com>
2022-05-23 15:45:53 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,37 +1,32 @@
-package employe;
+package briefpoo;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Employe employeF = new Employe("Mustapha", "Ihoum", "1997");
-		System.out.println(employeF);
-		
-		Employe employeC = new Employe("Mustapha", "Ihoum", "1997", calsalaire(4000));
-		System.out.println(employeC);
-		
-		Employe employeH = new Employe("Mustapha", "Ihoum", "1997");
-		System.out.println(employeH);
-		
-			/* TODO Auto-generated method stub
-			Employe emp = new Employe("Mustapha", "Ihoum", "1997");
-			
-			EmployeFix empf = new EmployeFix("Mustapha", "Ihoum", "1997", 8798.90);
-			
-			Employe EmployeComission = new Employe("Mustapha", "Ihoum", "1997");
-			EmployeCommission empC = new EmployeCommission("Mustapha", "Ihoum", "1997", 4000 + 5 * 10 );
-			
-			Employe EmployeHoraire = new Employe("Mustapha", "Ihoum", "1997");
-			EmployeHoraire empH= new EmployeHoraire("Mustapha", "Ihoum", "1997", 180*30 );
-
-			System.out.println(empf);
-			System.out.println(empC);
-			System.out.println(empH);
-			*/
-		}
-
-	private static Object calsalaire(int i) {
 		// TODO Auto-generated method stub
-		return null;
+		//Employe emp = new Employe("Mustapha", "Ihoum", "1997");
+		
+		EmployeFixe empf = new EmployeFixe(" Mustapha", " Ihoum", " 1997", 9000.00);
+		
+	 EmployeComission empc = new EmployeComission("Abdelali", " Charf", "1995", 6000, 20, 10);
+		//EmployeComission empC = new EmployeComission(" Nom : Mustapha", " Prenom : Ihoum", " Date : 1997", 4000 + 5 * 10 );
+		
+		
+	 EmployeHoraire empH= new EmployeHoraire("Omar", "Dbaa", "1994");
+		//EmployeHoraire empH= new EmployeHoraire(" Nom : Mustapha", " Prenom : Ihoum", " Date : 1997" );
+		System.out.println("***************Employee Fixe***************");
+		System.out.println();
+		System.out.println(empf.toString());
+		System.out.println();
+		System.out.println("***************Employee Commission***************");
+		System.out.println();
+		System.out.println(empc);
+		System.out.println();
+		System.out.println("***************Employee Horaire***************");
+		System.out.println();
+		System.out.println(empH);
+		System.out.println();
+		System.out.println("*****************************************");
 	}
-}
 
+}
