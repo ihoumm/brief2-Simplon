1e1b65a93e5295689b19f0d31a3b50257a6c4bb7
ihoumm <59344957+ihoumm@users.noreply.github.com>
2022-05-23 15:45:15 +0200

    Update EmployeHoraire.java

--- EmployeHoraire.java
+++ EmployeHoraire.java
@@ -1,24 +1,51 @@
-package employe;
+package briefpoo;
 
-public class EmployeHoraire extends Employe {
-
-	private int salaireHoraire;
+public class EmployeHoraire extends Employe{
+	private int nombreHeure = 1000;
+	private int tauxHoraire = 10;
+	//private double calsalaire;
 
 	public EmployeHoraire(String nom, String prenom, String date, int salaireHoraire ) {
 		super(nom, prenom, date);
+		
 	}
 
-	public double getSalaireHoraire() {
-		return salaireHoraire;
+
+	/**
+	 * @param nom
+	 * @param prenom
+	 * @param date
+	 */
+	public EmployeHoraire(String nom, String prenom, String date) {
+		super(nom, prenom, date);
+		// TODO Auto-generated constructor stub
+	}
+	public int getNombreHeure() {
+		return nombreHeure;
+	}
+	public void setNombreHeure(int nombreHeure) {
+		this.nombreHeure = nombreHeure;
+	}
+	public int getTauxHoraire() {
+		return tauxHoraire;
+	}
+	public void setTauxHoraire(int tauxHoraire) {
+		this.tauxHoraire = tauxHoraire;
+	}
+	@Override
+	public double calsalaire() {
+		// TODO Auto-generated method stub
+		return nombreHeure*tauxHoraire;
 	}
 
-	public void setSalaireHoraire(int salaireHoraire) {
-		this.salaireHoraire = salaireHoraire;
-	}
 
 	@Override
 	public String toString() {
-		return super.toString() +" salaireHoraire = "+super.calsalaire(salaireHoraire);
+		return "\r\n Nom :     " + super.getNom() + "\r\n Prenom :   " + getPrenom() + "\r\n Date :    " + getDate() +  "\r\n Salaire : "+ calsalaire() + " Dh";
 	}
+
+
+
+
 
 }
