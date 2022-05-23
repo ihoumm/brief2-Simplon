90e244433cb804476a2e6d570cb4a7426bc08d4e
ihoumm <59344957+ihoumm@users.noreply.github.com>
2022-05-23 15:42:58 +0200

    Update Employe.java

--- Employe.java
+++ Employe.java
@@ -1,53 +1,202 @@
-package employe;
+package briefpoo;
 
-public class Employe {
-	private String nom;
-	private String prenom;
-	private String date;
-	
-	public Employe(String nom, String prenom, String date) {
-		this.nom = nom;
-		this.prenom = prenom;
-		this.date = date;
-	}
-	
-	public Employe(String string, String string2, String string3, Object calsalaire) {
-		// TODO Auto-generated constructor stub
-	}
 
-	public int calsalaire(int salaireHoraire) {
-		return salaireHoraire;
-	}
-	
-	public Double employeC(double employeC) {
-		return employeC;
-	}
-	
-	public Double employeF(double employeF) {
-		return employeF;
-	}
-	
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	@Override
-	public String toString() {
-		return "[nom=" + nom + ", prenom=" + prenom + ", date=" + date;
-	}
-	
+public abstract class Employe {
+				
+				private String nom;
+				private String prenom;
+				private String date;
+				
+				public Employe(String nom, String prenom, String date) {
+					this.nom = nom;
+					this.prenom = prenom;
+					this.date = date;
+				}
+				
+				public abstract double calsalaire();
+				public String getNom() {
+					return nom;
+				}
+				public void setNom(String nom) {
+					this.nom = nom;
+				}
+				public String getPrenom() {
+					return prenom;
+				}
+				public void setPrenom(String prenom) {
+					this.prenom = prenom;
+				}
+				public String getDate() {
+					return date;
+				}
+				public void setDate(String date) {
+					this.date = date;
+				}
+				@Override
+				public String toString() {
+					return "" + nom + ", " + prenom + ", " + date;
+				}
+
+
+				
+				
 }
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
+				
+				//constructeur avec le parametre nom
+				/*public Employee(String nom, String prenom) {
+					super();
+					this.nom = nom;
+					this.prenom = prenom;
+				}
+			
+				public Employee(String nom, String prenom, String i, double salaireFixe, double salaireComission, double salaireHoraire) {
+					// TODO Auto-generated constructor stub
+					super();
+					this.nom = nom;
+					this.prenom = prenom;
+					this.date = i;
+					this.salaireFixe =salaireFixe;
+					this.salaireComission = salaireComission;
+					this.salaireHoraire = salaireHoraire;
+				}
+				public String getNom() {
+					return nom;
+					
+				}
+			
+				public void setNom(String nom) {
+					this.nom = nom;
+					
+				}
+				@Override
+				public String toString() {
+					return "Employee Fixe \r\n \r\n nom : " + nom + "\r\n prenom : " + prenom + " \r\n age : " + date  + " \r\n Salaire fixe : " + salaireFixe + " Dh ";
+				
+					
+				}
+					
+
+*/	
+
+package briefpoo;
+
+
+public abstract class Employe {
+				
+				private String nom;
+				private String prenom;
+				private String date;
+				
+				public Employe(String nom, String prenom, String date) {
+					this.nom = nom;
+					this.prenom = prenom;
+					this.date = date;
+				}
+				
+				public abstract double calsalaire();
+				public String getNom() {
+					return nom;
+				}
+				public void setNom(String nom) {
+					this.nom = nom;
+				}
+				public String getPrenom() {
+					return prenom;
+				}
+				public void setPrenom(String prenom) {
+					this.prenom = prenom;
+				}
+				public String getDate() {
+					return date;
+				}
+				public void setDate(String date) {
+					this.date = date;
+				}
+				@Override
+				public String toString() {
+					return "" + nom + ", " + prenom + ", " + date;
+				}
+
+
+				
+				
+}
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
+				
+				//constructeur avec le parametre nom
+				/*public Employee(String nom, String prenom) {
+					super();
+					this.nom = nom;
+					this.prenom = prenom;
+				}
+			
+				public Employee(String nom, String prenom, String i, double salaireFixe, double salaireComission, double salaireHoraire) {
+					// TODO Auto-generated constructor stub
+					super();
+					this.nom = nom;
+					this.prenom = prenom;
+					this.date = i;
+					this.salaireFixe =salaireFixe;
+					this.salaireComission = salaireComission;
+					this.salaireHoraire = salaireHoraire;
+				}
+				public String getNom() {
+					return nom;
+					
+				}
+			
+				public void setNom(String nom) {
+					this.nom = nom;
+					
+				}
+				@Override
+				public String toString() {
+					return "Employee Fixe \r\n \r\n nom : " + nom + "\r\n prenom : " + prenom + " \r\n age : " + date  + " \r\n Salaire fixe : " + salaireFixe + " Dh ";
+				
+					
+				}
+					
+
+*/	
+
